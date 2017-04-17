256a9bd13b210bb6bac2a08ff188be93a3911c08
victormunduruca <victormunduruca@gmail.com>
2017-04-17 10:17:49 -0300

    Bug data stream

--- Servidor/src/br/uefs/ecomp/servidor/controller/TrataClientes.java
+++ Servidor/src/br/uefs/ecomp/servidor/controller/TrataClientes.java
@@ -14,73 +14,77 @@ import br.uefs.ecomp.servidor.model.Pessoa;
 
 
 public class TrataClientes implements Runnable{
-
+	
+	ServerSocket servidor;
 	private Socket cliente; 
 	private Controller controller;
 	
 	public TrataClientes(ServerSocket servidor) {
-		controller = Controller.getInstance();
 		try {
 			this.cliente = servidor.accept();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		this.servidor = servidor;
+		controller = Controller.getInstance();
 	}
 
 	@Override
 	public void run() {
-		System.out.println("Nova conexão com o cliente " +cliente.getInetAddress().getHostAddress());
-		
-		
-		try {
-			
-			DataInputStream inputDados = new DataInputStream(cliente.getInputStream());
-			DataOutputStream outputDados = new DataOutputStream(cliente.getOutputStream());
-			
-			String pacote = (String) inputDados.readUTF();
-			System.out.println("pacote: " +pacote);
-			int acao =  retornaAcao(pacote);
-			System.out.println("Acao: " +acao);
-			
-			switch (acao) {
-			case 1:
-				int numeroConta = 0;
-				Pessoa novaPessoa = decodificaPessoa(pacote);
-				try {
-					controller.cadastrarNovaConta(novaPessoa, "corrente");	
-					outputDados.writeInt(Acao.CADASTRO_SUCESSO);
-					System.out.println(novaPessoa.getNome()+novaPessoa.getNumeroRegistro()+novaPessoa.getEndereco().getCep()+novaPessoa.getEndereco().getNumero()+novaPessoa.getEndereco().getRua()+novaPessoa.getUsuario()+novaPessoa.getSenha());
-					outputDados.close();
-				} catch (PessoaExistenteException e) {
-					outputDados.writeInt(10);
-					System.out.println("Cliente n cadastrado");
-				}
-				
-				
-				break;
-			case 2: 
-				Pessoa novaPessoaPoupanca = decodificaPessoa(pacote);
-				try {
-					controller.cadastrarNovaConta(novaPessoaPoupanca, "poupanca");	
-				} catch (PessoaExistenteException e) {
-					outputDados.writeInt(20);
-					e.printStackTrace();
-				}
-				outputDados.writeInt(Acao.CADASTRO_SUCESSO);
-				System.out.println(novaPessoaPoupanca.getNome()+novaPessoaPoupanca.getNumeroRegistro()+novaPessoaPoupanca.getEndereco().getCep()+novaPessoaPoupanca.getEndereco().getNumero()+novaPessoaPoupanca.getEndereco().getRua()+novaPessoaPoupanca.getUsuario()+novaPessoaPoupanca.getSenha());
-				outputDados.close();
-				break;
-			default:
-				break;
-			}
-			inputDados.close();
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
+		
+		while(true) {
+			try {
+				
+				System.out.println("Nova conexão com o cliente " +cliente.getInetAddress().getHostAddress());
+				DataInputStream inputDados = new DataInputStream(cliente.getInputStream());
+				DataOutputStream outputDados = new DataOutputStream(cliente.getOutputStream());
+				
+				String pacote = (String) inputDados.readUTF();
+				System.out.println("pacote: " +pacote);
+				int acao =  retornaAcao(pacote);
+				System.out.println("Acao: " +acao);
+				
+				switch (acao) {
+				case 1:
+					int numeroConta = 0;
+					Pessoa novaPessoa = decodificaPessoa(pacote);
+					try {
+						controller.cadastrarNovaConta(novaPessoa, "corrente");	
+						outputDados.writeInt(Acao.CADASTRO_SUCESSO);
+						System.out.println(novaPessoa.getNome()+novaPessoa.getNumeroRegistro()+novaPessoa.getEndereco().getCep()+novaPessoa.getEndereco().getNumero()+novaPessoa.getEndereco().getRua()+novaPessoa.getUsuario()+novaPessoa.getSenha());
+						outputDados.close();
+					} catch (PessoaExistenteException e) {
+						outputDados.writeInt(10);
+						System.out.println("Cliente n cadastrado");
+					}
+					
+					
+					break;
+				case 2: 
+					Pessoa novaPessoaPoupanca = decodificaPessoa(pacote);
+					try {
+						controller.cadastrarNovaConta(novaPessoaPoupanca, "poupanca");	
+					} catch (PessoaExistenteException e) {
+						outputDados.writeInt(20);
+						e.printStackTrace();
+					}
+					outputDados.writeInt(Acao.CADASTRO_SUCESSO);
+					System.out.println(novaPessoaPoupanca.getNome()+novaPessoaPoupanca.getNumeroRegistro()+novaPessoaPoupanca.getEndereco().getCep()+novaPessoaPoupanca.getEndereco().getNumero()+novaPessoaPoupanca.getEndereco().getRua()+novaPessoaPoupanca.getUsuario()+novaPessoaPoupanca.getSenha());
+					outputDados.close();
+					break;
+				default:
+					break;
+				}
+				inputDados.close();
+				cliente = servidor.accept();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	}
 	
 	public static int retornaAcao(String pacote) {
